e0459466ca84b7317a3cdd2964672dcc289acaca
Helloworld7-cyber <Matteobray77@gmail.com>
2025-12-11 12:04:33 +0100

    Update logout.php

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,8 +1,10 @@
 <?php
-// Supprime le cookie d'authentification
-setcookie('authToken', '', time() - 3600, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
+session_start();
 
-// Redirection vers la page de connexion
+// Supprime le cookie 'authToken' en le faisant expirer dans le passé
+setcookie('authToken', '', time() - 3600, '/', '', false, true);
+
+// Redirige vers la page de connexion
 header('Location: index.php');
 exit();
 ?>
